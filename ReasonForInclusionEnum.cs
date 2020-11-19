fd8e958f817934c57cad4697a29263fd83392e16
IvanZheleznyakov <iz14@yandex.ru>
2020-11-19 12:17:00 +0300

    потерявшийся reasonforinclusion

---
+++ ReasonForInclusionEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEDExplorer
+{
+    public enum ReasonForInclusionEnum
+    {
+        NOT_INCLUDED = 0,
+        DATA_CHANGE = 1,
+        QUALITY_CHANGE = 2,
+        DATA_UPDATE = 3,
+        INTEGRITY = 4,
+        GI = 5,
+        UNKNOWN = 6,
+    }
+}
