9ba267b9950b7ec1002552e0e97db4907af2475f
IvanZheleznyakov <iz14@yandex.ru>
2021-01-19 11:20:12 +0300

    правки по улучшению работы с головным проектом: работа с датасетами устройства; исправление багов в принятии сообщений; добавление логгера из головного проекта; и другое

---
+++ IedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lib61850net
+{
+    public class IedException : Exception
+    {
+        public IedException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ SourceMsgLogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DLL_Log;
+
+namespace lib61850net
+{
+
+}
