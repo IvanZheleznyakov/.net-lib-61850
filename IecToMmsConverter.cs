95c39f1abf9cbff917a7603d7c3e35afe1540ae5
IvanZheleznyakov <iz14@yandex.ru>
2021-03-17 10:02:52 +0300

    конвертация enum-string действительно должна быть проще (хех)

--- IecToMmsConverter.cs
+++ IecToMmsConverter.cs
@@ -6,6 +6,12 @@ namespace lib61850net
 {
     internal static class IecToMmsConverter
     {
+        /// <summary>
+        /// Изменение формата имени переменной из формата 61850 в формат MMS
+        /// </summary>
+        /// <param name="iecAddress">Полное имя переменной в терминах 61850</param>
+        /// <param name="FC">Функциональная связь</param>
+        /// <returns></returns>
         internal static string ConvertIecAddressToMms(string iecAddress, FunctionalConstraintEnum FC = FunctionalConstraintEnum.NONE)
         {
             string result = iecAddress.Replace('.', '$');
@@ -29,45 +35,7 @@ namespace lib61850net
 
         internal static string ConvertFCToString(FunctionalConstraintEnum FC)
         {
-            switch (FC)
-            {
-                case FunctionalConstraintEnum.BL: return "BL";
-                case FunctionalConstraintEnum.CF: return "CF";
-                case FunctionalConstraintEnum.CO: return "CO";
-                case FunctionalConstraintEnum.DC: return "DC";
-                case FunctionalConstraintEnum.EX: return "EX";
-                case FunctionalConstraintEnum.MX: return "MX";
-                case FunctionalConstraintEnum.OR: return "OR";
-                case FunctionalConstraintEnum.SE: return "SE";
-                case FunctionalConstraintEnum.SG: return "SG";
-                case FunctionalConstraintEnum.SP: return "SP";
-                case FunctionalConstraintEnum.SR: return "SR";
-                case FunctionalConstraintEnum.ST: return "ST";
-                case FunctionalConstraintEnum.SV: return "SV";
-                case FunctionalConstraintEnum.BR: return "BR";
-                case FunctionalConstraintEnum.RP: return "RP";
-                default: return "";
-            }
-            // .net standard 2.1
-            //return FC switch 
-            //{
-            //    FunctionalConstraintEnum.BL => "BL",
-            //    FunctionalConstraintEnum.CF => "CF",
-            //    FunctionalConstraintEnum.CO => "CO",
-            //    FunctionalConstraintEnum.DC => "DC",
-            //    FunctionalConstraintEnum.EX => "EX",
-            //    FunctionalConstraintEnum.MX => "MX",
-            //    FunctionalConstraintEnum.OR => "OR",
-            //    FunctionalConstraintEnum.SE => "SE",
-            //    FunctionalConstraintEnum.SG => "SG",
-            //    FunctionalConstraintEnum.SP => "SP",
-            //    FunctionalConstraintEnum.SR => "SR",
-            //    FunctionalConstraintEnum.ST => "ST",
-            //    FunctionalConstraintEnum.SV => "SV",
-            //    FunctionalConstraintEnum.BR => "BR",
-            //    FunctionalConstraintEnum.RP => "RP",
-            //    _ => "",
-            //};
+            return FC == FunctionalConstraintEnum.NONE ? "" : FC.ToString();
         }
     }
 }
